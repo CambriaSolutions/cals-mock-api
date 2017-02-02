fb91d85fabfd8bebe302de53b503dce4f1f9b69e
Ratnesh Raval <rraval@cambriasolutions.com>
2017-02-02 10:33:50 -0800

    Facility model mock

---
+++ spec/factories/facilities.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :facility do
+    name "MyString"
+    number "MyString"
+    admin_name "MyString"
+    capacity 1
+    approval_date "2017-02-01 16:30:50"
+  end
+end
