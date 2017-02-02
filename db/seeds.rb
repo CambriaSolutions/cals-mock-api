5a0434d17a06e8d1741d00417ca7b44cac214cca
Ratnesh Raval <rraval@cambriasolutions.com>
2017-02-02 13:19:41 -0800

    Facilities seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  Facility.create!(
+    name: Faker::Lorem.sentence(1, true, 2),
+    number: Faker::Number.number(8),
+    admin_name: Faker::Name.name,
+    capacity: Faker::Number.number(2),
+    approval_date: Faker::Date.between(20.year.ago, Date.today)
+  )
+end
+
+p "Facilties created"
