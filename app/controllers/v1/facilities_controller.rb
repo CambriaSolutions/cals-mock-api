7a5354f2df169d15d97b955590c772d711cb015c
Lavanya Reddy <lreddy@cambriasolutions.com>
2017-02-05 21:48:31 -0800

    Add create, update and destroy actions

--- app/controllers/v1/facilities_controller.rb
+++ app/controllers/v1/facilities_controller.rb
@@ -1,4 +1,5 @@
 class V1::FacilitiesController < ApplicationController
+  before_action :find_facility, only: [:show, :destroy, :update]
 
   # GET /v1/facilities
   def index
@@ -6,10 +7,32 @@ class V1::FacilitiesController < ApplicationController
     json_response(@facilities)
   end
 
-
   # GET /v1/facilities/:id
   def show
-    @faciltiy = Facility.find(params[:id])
-    json_response(@faciltiy)
+    json_response(@facility)
   end
+
+  def update
+    @facility.update_attributes(facility_params)
+    json_response(@facility)
+  end
+
+  def create
+    facility = Facility.new
+    facility.update_attributes(facility_params)
+    json_response(facility)
+  end
+
+  def destroy
+    @facility.destroy
+  end
+
+  private
+    def find_facility
+      @facility = Facility.find(params[:id])
+    end
+
+    def facility_params
+      params.permit(:name, :admin_name, :capacity, :number, :approval_date)
+    end
 end
