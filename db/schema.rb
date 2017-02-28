7e3609e40188d2977fd66f76746d4f77c4e19bad
Ratnesh Raval <rraval@cambriasolutions.com>
2017-02-28 09:17:39 -0800

    Removing facilitiy migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,3 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202003050) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "facilities", force: :cascade do |t|
-    t.string   "name"
-    t.string   "number"
-    t.string   "admin_name"
-    t.integer  "capacity"
-    t.datetime "approval_date"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-  end
-
-end
