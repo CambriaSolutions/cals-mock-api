1218e29781a69d92ec8a672570ddd7bf53a83c4a
Lavanya Reddy <lreddy@cambriasolutions.com>
2017-04-04 14:01:51 -0700

    search for facility

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -13,6 +13,22 @@ class Facility < ActiveRecord::Base
   belongs_to :last_visit_reason_mapping, :class_name => 'CodeMapping::LastVisitReason', :foreign_key => 'fac_last_visit_reason'
   belongs_to :assigned_worker_mapping, :class_name => 'CodeMapping::AssignedWorker', :foreign_key => 'fac_do_eval_code'
 
+  mapping dynamic: 'false' do
+    indexes :fac_res_street_addr, type: 'text'
+    indexes :fac_res_city, type: 'text'
+    indexes :fac_res_state, type: 'text'
+    indexes :fac_res_zip_code, type: 'text'
+    indexes :fac_type
+    indexes :fac_nbr
+    indexes :fac_status
+    indexes :fac_co_nbr
+
+    indexes :type_mapping do
+      indexes :value, type: 'text',
+       "index": 'not_analyzed'
+    end
+  end
+
   def county
     county_mapping&.value
   end
@@ -40,8 +56,8 @@ class Facility < ActiveRecord::Base
   def self.retrieve_search_results(query)
     Facility.search query: {multi_match: {query: query,
                                           type: 'cross_fields',
-                                          minimum_should_match: '80%',
-                                          fields: ['fac_res_street_addr','fac_res_city','fac_res_state','fac_res_zip_code'],
+                                          minimum_should_match: '50%',
+                                          fields: ['fac_nbr', 'fac_status', 'fac_co_nbr', 'fac_type', 'fac_res_street_addr','fac_res_city', 'fac_res_state'],
                                           lenient: true}}
   end
 end
